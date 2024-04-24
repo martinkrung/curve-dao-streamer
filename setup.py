338bd772a8fcc658bf305133118cf1cdee2418dd
Martin Krung <mk@cryptonative.ch>
2024-04-24 16:30:32 +0200

    chore: add setup for test vars

---
+++ setup.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from setuptools import find_packages, setup
+
+extras_require = {
+    "test": [  # `test` GitHub Action jobs uses this
+        "pytest>=6.0",  # Core testing package
+        "pytest-xdist",  # Multi-process runner
+        "pytest-cov",  # Coverage analyzer plugin
+        "hypothesis>=6.2.0,<7.0",  # Strategy-based fuzzer
+    ],
+    "lint": [
+        "black>=24.2.0,<24",  # Auto-formatter and linter
+        "mypy>=1.8.0,<2",  # Static type analyzer
+        "types-setuptools",  # Needed for mypy type shed
+        "types-requests",  # Needed for mypy type shed
+        "flake8>=7.0.0,<8",  # Style linter
+        "flake8-breakpoint>=1.1.0,<2",  # Detect breakpoints left in code
+        "flake8-print>=5.0.0,<6",  # Detect print statements left in code
+        "isort>=5.10.1,<6",  # Import sorting linter
+        "mdformat>=0.7.17",  # Auto-formatter for markdown
+        "mdformat-gfm>=0.3.5",  # Needed for formatting GitHub-flavored markdown
+        "mdformat-frontmatter>=0.4.1",  # Needed for frontmatters-style headers in issue templates
+        "mdformat-pyproject>=0.0.1",  # Allows configuring in pyproject.toml
+    ],
+    "release": [  # `release` GitHub Action job uses this
+        "setuptools",  # Installation tool
+        "wheel",  # Packaging tool
+        "twine",  # Package upload tool
+    ],
+    "dev": [
+        "commitizen",  # Manage commits and publishing releases
+        "pre-commit",  # Ensure that linters are run prior to committing
+        "pytest-watch",  # `ptw` test watcher/runner
+        "IPython",  # Console for interacting
+        "ipdb",  # Debugger (Must use `export PYTHONBREAKPOINT=ipdb.set_trace`)
+    ],
+}
+
+# NOTE: `pip install -e .[dev]` to install package
+extras_require["dev"] = (
+    extras_require["test"]
+    + extras_require["lint"]
+    + extras_require["release"]
+    + extras_require["dev"]
+)
+
+with open("./README.md") as readme:
+    long_description = readme.read()
